7215a427853585215510a389b846cec55f573f71
Kire Lalkov <kire.lalkov@sorsix.com>
2024-04-22 22:15:24 +0200

    Inital commit

---
+++ Backend/raeda/src/main/kotlin/com/sorsix/raeda/RaedaApplication.kt
@@ -0,0 +1,11 @@
+package com.sorsix.raeda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RaedaApplication
+
+fun main(args: Array<String>) {
+    runApplication<RaedaApplication>(*args)
+}
