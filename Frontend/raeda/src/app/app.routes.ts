e3b7171e0e6e94d1547020c2b60cc5420be998fc
Hristijan Gjorgjiev <hristijangjorgjiev@Hristijans-MacBook-Air.local>
2024-04-29 03:09:50 +0200

    added guard to sign-in and sign-up pages

--- Frontend/raeda/src/app/app.routes.ts
+++ Frontend/raeda/src/app/app.routes.ts
@@ -3,16 +3,19 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
+import { signInGuard } from './guards/sign-in.guard';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent, pathMatch: 'full' },
   {
     path: 'signin',
     component: SignInPageComponent,
+    canActivate: [signInGuard],
   },
   {
     path: 'signup',
     component: SignUpPageComponent,
+    canActivate: [signInGuard],
   },
   {
     path: 'cars',
---
+++ Frontend/raeda/src/app/guards/sign-in.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { BrowserStorageService } from '../browserStorage.service';
+import { map, take } from 'rxjs';
+
+export const signInGuard: CanActivateFn = (route, state) => {
+  const browserStorageService = inject(BrowserStorageService);
+  const router = inject(Router);
+
+  return browserStorageService.isSignIn.pipe(
+    take(1),
+    map((status) => {
+      if (status) return router.createUrlTree(['/']);
+
+      return true;
+    })
+  );
+};
