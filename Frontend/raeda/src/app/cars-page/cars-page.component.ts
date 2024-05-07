08b7c8a1f4adad657f604f66674958362c4599a3
Hristijan Gjorgjiev <hristijangjorgjiev@Hristijans-MacBook-Air.local>
2024-05-07 14:44:48 +0200

    filter sidebar separate to a component

--- Frontend/raeda/src/app/cars-page/cars-page.component.ts
+++ Frontend/raeda/src/app/cars-page/cars-page.component.ts
@@ -4,20 +4,10 @@ import { FooterComponent } from '../components/footer/footer.component';
 import { CarService } from '../shared/car.service';
 import { CarModel } from '../shared/car.model';
 import { CarCardComponent } from '../components/car-card/car-card.component';
-import { SidebarModule } from 'primeng/sidebar';
 import { LoadingComponent } from '../components/loading/loading.component';
-import { AccordionModule } from 'primeng/accordion';
 import { ErrorComponent } from '../components/error/error.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { SliderModule } from 'primeng/slider';
-import { ListboxModule } from 'primeng/listbox';
-import { CalendarModule } from 'primeng/calendar';
-import { InputSwitchModule } from 'primeng/inputswitch';
-
-interface Price {
-  title: string;
-  amount: number;
-}
+import { FilterSidebarComponent } from '../components/filter-sidebar/filter-sidebar.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-cars-page',
@@ -26,15 +16,9 @@ interface Price {
     NavbarComponent,
     FooterComponent,
     CarCardComponent,
-    SidebarModule,
     LoadingComponent,
     ErrorComponent,
-    AccordionModule,
-    ReactiveFormsModule,
-    SliderModule,
-    ListboxModule,
-    CalendarModule,
-    InputSwitchModule,
+    FilterSidebarComponent,
   ],
   templateUrl: './cars-page.component.html',
   styleUrl: './cars-page.component.css',
@@ -42,27 +26,11 @@ interface Price {
 export class CarsPageComponent {
   cars: CarModel[] = [];
   carService = inject(CarService);
-  toggleFilterBy = false;
   isLoading = false;
   error = false;
-  form: FormGroup = new FormGroup({});
-  locations = ['All', 'Skopje', 'Strumica', 'Kavadarci'];
-  prices: Price[] = [
-    { title: 'All', amount: 0 },
-    { title: 'Under 200€', amount: 200 },
-    { title: 'Under 400€', amount: 400 },
-    { title: 'Under 600€', amount: 600 },
-    { title: 'Under 800€', amount: 800 },
-    { title: 'Under 1000€', amount: 1000 },
-  ];
-  brands = ['Porsche', 'Audi', 'Mercedes', 'BMW', 'Ford', 'Toyota'];
-  years = [2017, 2018, 2019, 2020, 2021, 2022, 2023];
-  fuels = ['All', 'Petrol', 'Diesel'];
-  gears = ['All', 'Manuel', 'Automatic'];
+  toggleFilterBy = new BehaviorSubject<boolean>(false);
 
   ngOnInit(): void {
-    this.form = this.initForm();
-
     this.isLoading = true;
     this.error = false;
     this.carService.getAllCars().subscribe({
@@ -78,19 +46,6 @@ export class CarsPageComponent {
   }
 
   onToggleFilterBy() {
-    this.toggleFilterBy = !this.toggleFilterBy;
-  }
-
-  initForm() {
-    return new FormGroup({
-      selectedLocation: new FormControl(this.locations[0]),
-      selectedPickupDate: new FormControl(null),
-      selectedPrice: new FormControl(this.prices[0]),
-      selectedBrands: new FormControl(this.brands),
-      selectedYears: new FormControl(this.years),
-      selectedFuel: new FormControl(this.fuels[0]),
-      selectedGear: new FormControl(this.gears[0]),
-      selectedAvailability: new FormControl(null),
-    });
+    this.toggleFilterBy.next(true);
   }
 }
