9cb0944596a45396173d268ab20bbed188372aa8
Hristijan Gjorgjiev <hristijangjorgjiev@Hristijans-MacBook-Air.local>
2024-05-19 19:13:21 +0200

    change role on rental endpoint

--- Backend/raeda/src/main/kotlin/com/sorsix/raeda/config/SecurityConfiguration.kt
+++ Backend/raeda/src/main/kotlin/com/sorsix/raeda/config/SecurityConfiguration.kt
@@ -17,7 +17,10 @@ class SecurityConfiguration(
 ) {
 
     @Bean
-    fun securityFilterChain(http: HttpSecurity, jwtAuthenticationFilter: JwtAuthenticationFilter): DefaultSecurityFilterChain {
+    fun securityFilterChain(
+        http: HttpSecurity,
+        jwtAuthenticationFilter: JwtAuthenticationFilter
+    ): DefaultSecurityFilterChain {
         http
             .csrf { it.disable() }
             .authorizeHttpRequests {
@@ -28,8 +31,8 @@ class SecurityConfiguration(
                     .permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/cars/**", "/api/loc")
                     .permitAll()
-                    .requestMatchers(HttpMethod.GET,  "/api/user/rentals/{id}", "/api/user/rentals")
-                    .hasRole("USER")
+                    .requestMatchers(HttpMethod.GET, "/api/user/rentals/{id}", "/api/user/rentals")
+                    .hasAnyRole("ADMIN", "USER")
                     .requestMatchers("/api/user**", "/api/cars/edit**")
                     .hasRole("ADMIN")
                     .anyRequest()
